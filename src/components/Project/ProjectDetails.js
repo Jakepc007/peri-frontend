86a86c45094b26c1f8646ccf629ab29ef5ccf8ac
Jake Morris <jakemorrishello@gmail.com>
2021-03-25 10:41:23 +0000

    added files from repo

--- src/components/Project/ProjectDetails.js
+++ src/components/Project/ProjectDetails.js
@@ -1,36 +1,74 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Box, Container, Flex, Heading, Text } from "@chakra-ui/layout";
 import Timeline from "../Events/Timeline";
 import ProjectService from "../../services/project.service";
 import { SeparatedHeading } from "../Util/SeparatedHeading/SeparatedHeading";
+import UpdateStatus from "../Events/UpdateStatus";
+import AssignEngineers from "../Events/AssigingEngineers/AssignEngineers";
 
 const ProjectDetails = (props) => {
-    const projectId = props.match.params.param1;
+    let projectId = useRef(props.match.params.param1);
     const [project, setProject] = useState(props.project);
 
     useEffect(() => {
-        if (projectId !== undefined) {
-            ProjectService.getProjectByID(projectId).then((projectReturned) => {
-                setProject(projectReturned);
-            });
+        if (projectId.current !== undefined) {
+            ProjectService.getProjectByID(projectId.current).then(
+                (projectReturned) => {
+                    setProject(projectReturned);
+                }
+            );
         }
-    }, [projectId]);
+    }, [projectId.current]);
 
+    const updateProjectDisplayed = (project) => {
+        setProject(project);
+    };
 
     return (
         <Container maxW="6xl" marginTop={12} marginBottom={12}>
-            <SeparatedHeading primary='Project Details' secondary='Manage or View your Project' />
+            <SeparatedHeading
+                primary="Project Details"
+                secondary="Manage or View your Project"
+            />
 
             <Flex bg={"brand.background"} borderRadius="lg" boxShadow="lg">
-                {projectId ? (
+                {projectId.current ? (
                     <>
                         <Box marginBottom={1}>
                             <Box m={10}>
-                                {project ?
+                                {project ? (
                                     <Box marginBottom={8}>
-                                        <Heading> {project.name}  <Text color='grey'>#{project.number}</Text> </Heading>
+                                        <Flex>
+                                            <Heading w={"100%"}>
+                                                {" "}
+                                                {project.name}{" "}
+                                                <Text color="grey">
+                                                    #{project.number}
+                                                </Text>{" "}
+                                            </Heading>
+                                            <UpdateStatus
+                                                count={1000}
+                                                projectStatus={
+                                                    project.status.value
+                                                }
+                                                projectId={projectId.current}
+                                                updateParent={
+                                                    updateProjectDisplayed
+                                                }
+                                                inMenu={false}
+                                            />
+                                            <AssignEngineers
+                                                updateParent={
+                                                    updateProjectDisplayed
+                                                }
+                                                project={project}
+                                                inMenu={false}
+                                            />
+                                        </Flex>
                                     </Box>
-                                    : <></>}
+                                ) : (
+                                    <></>
+                                )}
                                 <Timeline project={project} />
                             </Box>
                             {project ? (
@@ -49,34 +87,58 @@ const ProjectDetails = (props) => {
                                             <Heading as="h4" size="md" mt={3}>
                                                 Description
                                             </Heading>
-                                            <Text>
-                                                {project.description}
-                                            </Text>
+                                            <Text>{project.description}</Text>
 
                                             <Heading as="h4" size="md" mt={3}>
                                                 Client
                                             </Heading>
-                                            <Text>
-                                                {project.client}
-                                            </Text>
+                                            <Text>{project.client}</Text>
 
                                             <Heading as="h4" size="md" mt={3}>
                                                 System
                                             </Heading>
-                                            <Text>
-                                                {project.system}
-                                            </Text>
+                                            <Text>{project.system}</Text>
 
                                             <Heading as="h4" size="md" mt={3}>
                                                 Sector
                                             </Heading>
+                                            <Text>{project.sector}</Text>
+                                            <Heading as="h4" size="md" mt={3}>
+                                                Technical Team Lead
+                                            </Heading>
                                             <Text>
-                                                {project.sector}
+                                                {project.engineers
+                                                    .technical_lead_id
+                                                    .firstname +
+                                                    " " +
+                                                    project.engineers
+                                                        .technical_lead_id
+                                                        .lastname}
                                             </Text>
-
+                                            <Heading as="h4" size="md" mt={3}>
+                                                Design Engineer
+                                            </Heading>
+                                            <Text>
+                                                {project.engineers.designer_id
+                                                    .firstname +
+                                                    " " +
+                                                    project.engineers
+                                                        .designer_id.lastname}
+                                            </Text>
+                                            <Heading as="h4" size="md" mt={3}>
+                                                Design Checker
+                                            </Heading>
+                                            <Text>
+                                                {project.engineers
+                                                    .design_checker_id
+                                                    .firstname +
+                                                    " " +
+                                                    project.engineers
+                                                        .design_checker_id
+                                                        .lastname}
+                                            </Text>
                                         </Box>
                                     </Box>
-
                                 </>
                             ) : (
                                 <> </>
@@ -88,7 +150,6 @@ const ProjectDetails = (props) => {
                 )}
             </Flex>
         </Container>
-
     );
 };
 
